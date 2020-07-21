89a56982a70952b82ab5cffd691ac6e5bebbde4f
grossrob0 <66632141+grossrob0@users.noreply.github.com>
2020-07-21 13:07:37 -0400

    Add files via upload

---
+++ gdbe/offline.js
@@ -0,0 +1,29 @@
+﻿{
+	"version": 1595350786,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/playerhitbox-sheet0.png",
+		"images/player-sheet0.png",
+		"images/g.png",
+		"images/bg.png",
+		"images/particles.png",
+		"images/line-sheet0.png",
+		"images/camera-sheet0.png",
+		"images/title-sheet0.png",
+		"images/title2-sheet0.png",
+		"images/button-sheet0.png",
+		"images/img-sheet0.png",
+		"images/bg2.png",
+		"images/tilemap.png",
+		"images/button2.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
